7de647df0dccdafa25adcff40c97898601c3abb9
drkspark <madhusudan753@gmail.com>
2023-02-01 08:31:05 +0530

    Added Roles and user_roles throught table for many-to-many relationship between Role and User

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { PORT } = require('./config/serverConfig');
-const apiRoutes = require('./routes/index');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index");
+
+const db = require("./models/index");
+const { User, Role } = require("./models/index");
 
 const app = express();
 
 const prepareAndStartServer = () => {
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.use('/api', apiRoutes);
+    app.use("/api", apiRoutes);
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
+
+        if (process.env.DB_Sync) {
+            db.sequelize.sync({ alter: true });
+        }
+
+        // const u1 = await User.findByPk(3);
+        // const r1 = await Role.findByPk(1);
+        // u1.addRole(r1);
+        // const response = await u1.getRoles();
+        // const response = await r1.getUsers();
+        // const response = await u1.hasRole(r1);
+
+        // console.log(response);
+
         // const service = new UserService();
         // const newToken = service.createToken({email: 'drkspark@hotmail.com', id: 2});
         // console.log(newToken);
@@ -21,6 +38,6 @@ const prepareAndStartServer = () => {
         // const veri = service.verifyToken(token);
         // console.log(veri);
     });
-}   
+};
 
 prepareAndStartServer();
